b33f7711e4632a5a4ab585cbfb6bd7190d758882
0x6e <0x6e@users.noreply.github.com>
2016-03-15 18:13:10 +0000

    Fixed dev camera

--- python/camera-stream/camera_dev/camera.py
+++ python/camera-stream/camera_dev/camera.py
@@ -6,7 +6,11 @@ class Camera(object):
     files 1.jpg, 2.jpg and 3.jpg at a rate of one frame per second."""
 
     def __init__(self):
-        self.frames = [open(f + '.jpg', 'rb').read() for f in ['1', '2', '3']]
+        print("Reading example frames...")
+        self.frames = [open('camera_dev/' + f + '.jpg', 'rb').read() for f in ['1', '2', '3']]
+        print("... done!")
 
     def get_frame(self):
-        return self.frames[int(time()) % 3]
+        i = int(time()) % 3
+        #print("Get frame {}".format(i))
+        return self.frames[i]

5402a2b23fad73e82cf217d0cf07c2805928a6ec
0x6e <0x6e@users.noreply.github.com>
2016-03-15 21:34:02 +0000

    Added basic thread to parse instructions on

--- python/camera-stream/rover_interface.py
+++ python/camera-stream/rover_interface.py
@@ -1,10 +1,15 @@
 import json
+import threading
 from collections import deque
 
 class RoverInterface:
 
-    def __init__(self):
-        self.instructionDeque = deque()
+    # The instructions to parse
+    instructionDeque = deque()
+
+    # The thread that passes the instructions to the Arduino.
+    thread = None
+
 
     @classmethod
     def validCommands(self):
@@ -35,5 +40,21 @@ class RoverInterface:
 
     def addValidInstructions(self, instructions):
         """Assumes the instructions are valid"""
-        self.instructionDeque.extend(instructions)
-        print(self.instructionDeque)
+        RoverInterface.instructionDeque.extend(instructions)
+
+        # If the thread is not running, create a new one and start it
+        # TODO: is this safe? - Probably not
+        if RoverInterface.thread is None:
+            RoverInterface.thread = threading.Thread(target=self._thread)
+            RoverInterface.thread.start()
+
+
+    @classmethod
+    def _thread(cls):
+        """This method is run by the thread. It runs the instructions on the Arduino."""
+        while len(cls.instructionDeque) > 0:
+            instruction = cls.instructionDeque.popleft()
+            print(instruction['command'])
+
+        # TODO: This is only reassigning the reference, not deleting the thread
+        cls.thread = None
